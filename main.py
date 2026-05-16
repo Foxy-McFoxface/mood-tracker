30e9320a7d2d0c5e429ee5f1f11f20488aa32c00
Your Name <primoz.arko@protonmail.com>
2026-05-16 09:04:51 +0200

    mood tracker

--- main.py
+++ main.py
@@ -3,18 +3,17 @@ mood = input("How are you feeling today? ")
 message = name + " feels " + mood + " today.\n"
 date = input("What is the date today? (MM/DD/YYYY) ")
 message = date + ": " + message
-
 mood_score = 0
 if mood == "happy":
-    mood_score = 5
+    mood_score = 80
 elif mood == "sad":
-    mood_score = 1  
+    mood_score = 1
 elif mood == "neutral":
-    mood_score = 3
+    mood_score = 50
 elif mood == "excited":
-    mood_score = 4
+    mood_score = 100
 elif mood == "angry":
-    mood_score = 2
+    mood_score = 20
 message = message + " Mood Score: " + str(mood_score) + "\n"
 
 file = open("mood.txt", "a")
